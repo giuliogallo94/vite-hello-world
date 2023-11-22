057ab778674677de9059400dcae765daafa0ec3b
giuliogallo94 <giuliogallo1994@gmail.com>
2023-11-22 13:29:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Title from "./components/AppTitle.vue";
+
+export default {
+  components: {
+    Title,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <Title />
+</template>
+
+<style scoped></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      text: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background-color: aqua;
+  width: 100vw;
+  height: 100vh;
+  text-align: center;
+  line-height: 100vh;
+}
+</style>
